76e85dad92b0c66111ef75db8694c861597c438e
Krzysztof Borowy <6444719+krizzu@users.noreply.github.com>
2025-05-16 20:52:47 +0200

    chore: drop not used

--- sample-app/src/commonMain/kotlin/com/kborowy/kolorpicker/sample/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.kborowy.kolorpicker.sample
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- sample-app/src/desktopMain/kotlin/com/kborowy/kolorpicker/sample/Platform.jvm.kt
+++
@@ -1,7 +0,0 @@
-package com.kborowy.kolorpicker.sample
-
-class JVMPlatform: Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()

3a7d226b60f6bffc49a8889336022ccab3c11453
Krzysztof Borowy <6444719+krizzu@users.noreply.github.com>
2025-05-16 20:53:59 +0200

    chore: update sample App

--- sample-app/src/commonMain/kotlin/com/kborowy/kolorpicker/sample/App.kt
+++ sample-app/src/commonMain/kotlin/com/kborowy/kolorpicker/sample/App.kt
@@ -3,6 +3,7 @@ package com.kborowy.kolorpicker.sample
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -27,10 +29,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.kborowy.colorpicker.KolorPicker
 import com.kborowy.colorpicker.ext.toHex
-import org.jetbrains.compose.ui.tooling.preview.Preview
 import kotlin.random.Random
 import kotlin.random.nextInt
-
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
@@ -44,6 +45,7 @@ fun App() {
             )
         )
     }
+    var visible by remember { mutableStateOf(true) }
 
     MaterialTheme {
         Column(
@@ -51,22 +53,41 @@ fun App() {
                 .windowInsetsPadding(WindowInsets.safeContent).padding(top = 50.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            KolorPicker(
-                initialColor = selectedColor,
-                onColorSelected = { selectedColor = it },
-                modifier = Modifier.width(350.dp).height(300.dp).clip(RoundedCornerShape(16.dp))
-                    .padding(10.dp),
-            )
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Column {
+                    Button(onClick = { visible = !visible }) {
+                        Text("Toggle visibility")
+                    }
+
+                    Button(enabled = !visible, onClick = { selectedColor = Color.random() }) {
+                        Text("Random initial")
+                    }
+
+                }
+
+                Spacer(modifier = Modifier.width(10.dp))
+
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text("#${selectedColor.toHex()}", color = Color.White)
+                    Box(modifier = Modifier.size(100.dp).background(selectedColor))
+                }
+            }
+
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            if (visible) {
+                KolorPicker(
+                    initialColor = selectedColor,
+                    onColorSelected = { selectedColor = it },
+                    modifier = Modifier.width(350.dp).height(300.dp).clip(RoundedCornerShape(16.dp))
+                        .padding(10.dp),
+                )
+            }
 
             Spacer(modifier = Modifier.height(50.dp))
 
-
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text("picked color: #${selectedColor.toHex()}", color = Color.White)
-                Box(modifier = Modifier.size(100.dp).background(selectedColor))
-            }
-
-            Spacer(modifier = Modifier.height(10.dp))
         }
     }
 }
